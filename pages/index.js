62a1fa0d03668d5354c25c34bcd5753e7e3f03e4
anxhirr <anxhirr@gmail.com>
2023-01-25 09:40:13 +0100

    progressing

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: ' A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
+    address: 'Some address 1, 123456 Some City',
+    description: 'This is first meetup!'
+
+  },
+  {
+    id: 'm2',
+    title: ' A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
+    address: 'Some address 1, 123456 Some City',
+    description: 'This is second meetup!'
+
+  },
+  {
+    id: 'm3',
+    title: ' A Third Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
+    address: 'Some address 1, 123456 Some City',
+    description: 'This is third meetup!'
+
+  },
+]
+
+const HomePage = () => {
+  return (
+    <MeetupList meetups={DUMMY_MEETUPS} />
+  )
+}
+export default HomePage
