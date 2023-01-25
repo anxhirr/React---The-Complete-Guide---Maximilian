176d858d5d9e024e30787fb413c7327fedf64410
anxhirr <anxhirr@gmail.com>
2023-01-25 23:46:04 +0100

    added meetup detail

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg'
+      title='First Meetup'
+      address='Some address 1, 123456 Some City'
+      description='This is a first meetup '
+    />
+  )
+}
+export default MeetupDetails
--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,30 @@ const DUMMY_MEETUPS = [
   {
     id: 'm1',
     title: ' A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
     address: 'Some address 1, 123456 Some City',
-    description: 'This is first meetup!'
-
+    description: 'This is first meetup!',
   },
   {
     id: 'm2',
     title: ' A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
     address: 'Some address 1, 123456 Some City',
-    description: 'This is second meetup!'
-
+    description: 'This is second meetup!',
   },
   {
     id: 'm3',
     title: ' A Third Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/bc/Stanford_Memorial_Church_October_2019_HDR.jpg',
     address: 'Some address 1, 123456 Some City',
-    description: 'This is third meetup!'
-
+    description: 'This is third meetup!',
   },
 ]
 
 const HomePage = () => {
-  return (
-    <MeetupList meetups={DUMMY_MEETUPS} />
-  )
+  return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 export default HomePage
